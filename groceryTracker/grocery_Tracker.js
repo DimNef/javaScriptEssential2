38a45cf9da6fe5238a7ce9a7e92e40e856f26883
DimNef <dimkrakis@gmail.com>
2024-10-22 17:41:41 -0400

    grocery tracker 3

--- groceryTracker/grocery_Tracker.js
+++ groceryTracker/grocery_Tracker.js
@@ -3,12 +3,13 @@ let grocery2;
 let grocery3;
 
 function calculateTotalAmount() {
-    grocery1 = parseFloat(document.getElementById('grocery1').value);
-    grocery2 = parseFloat(document.getElementById('grocery2').value);
-    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+  if (isNaN((grocery1 = parseFloat(document.getElementById('grocery1').value)))){grocery1 = 0;}
+  if (isNaN((grocery2 = parseFloat(document.getElementById('grocery2').value)))){grocery2 = 0;}
+  if (isNaN((grocery3 = parseFloat(document.getElementById('grocery3').value)))){grocery3 = 0}
 
 let total = grocery1 + grocery2 + grocery3;
-console.log(total);
+console.log(grocery1);
 document.getElementById('result').innerText = `The total amount is: $ ${total}`;
 
 }
