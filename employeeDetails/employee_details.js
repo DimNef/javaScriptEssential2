f11561d6003f59b96bdad76ecd3ff8cc16ca43a4
DimNef <dimkrakis@gmail.com>
2024-10-23 19:12:54 -0400

    employ 2

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,9 +1,9 @@
 const employees = [
-    { id: 1, name:'John', age:30, department:'IT', salary:5000 },
-    { id: 2, name:'Alice', age:24, department:'HR', salary:6000 },
-    { id: 3, name:'Mary', age:35, department:'Finance', salary:5600 },
-    { id: 4, name:'Kostas', age:40, department:'Marketing', salary:5900 },
-    { id: 5, name:'Dim', age:33, department:'HR', salary:10000 }
+    { id: 1, name:'John', age:30, department:'IT', salary:5000, specialty: 'Python' },
+    { id: 2, name:'Alice', age:24, department:'HR', salary:6000, specialty: 'Javascript' },
+    { id: 3, name:'Mary', age:35, department:'Finance', salary:5600, specialty: 'Python' },
+    { id: 4, name:'Kostas', age:40, department:'Marketing', salary:5900, specialty: 'Python' },
+    { id: 5, name:'Dim', age:33, department:'HR', salary:10000, specialty: 'C++' }
 ]
 
 displayEmployees();
@@ -33,4 +33,13 @@ function findEmployeeById(employeeId) {
     } else {
         document.getElementById('employeesDetails').innerHTML = 'no employee found with this id';
     }
+}
+
+function findEmployeeBySpecialization(value) {
+    console.log('here');
+   // const selectedSpecialization = document.getElementById('spesialization').value;
+    const selectedSpecialization = value;
+    const foundEmployees = employees.filter(employee => employee.specialty == selectedSpecialization);
+    const foundEmployeesDisplay = foundEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.department} - $${employee.salary}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = foundEmployeesDisplay;
 }
